ef0c4d86d77046f4824d98edf4ec7fe6fe94b4f7
Jesse Newland <jnewland@gmail.com>
2010-01-07 16:54:58 -0500

    send USR1 to searchd on log rotation

--- lib/sphinx.rb
+++ lib/sphinx.rb
@@ -28,6 +28,12 @@ module Sphinx
       :cwd => '/tmp',
       :require => package('wget'),
       :unless => "test -f /usr/local/bin/searchd && test #{options[:version]} = `searchd --help | grep Sphinx | awk '{print $2}' | awk -F- '{print $1}'`"
+
+    configure(:rails_logrotate => {})
+    postrotate = configuration[:rails_logrotate][:postrotate] || "touch #{configuration[:deploy_to]}/current/tmp/restart.txt"
+    configure(:rails_logrotate => {
+      :postrotate => "#{postrotate}\n    pkill -USR1 searchd"
+     })
   end
   
 end
