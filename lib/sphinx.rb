50d467964c02819409c57e0a8de0c1749b3a072b
Josh Nichols <josh@technicalpickles.com>
2011-03-19 15:09:36 -0400

    Don't recursively change permissions on sphinx directory.

--- lib/sphinx.rb
+++ lib/sphinx.rb
@@ -51,7 +51,6 @@ module Sphinx
       raise "Expected #{sphinx_yml} to set '#{config}' for '#{rails_env}', but it did not. A decent value to set it to is: #{configuration[:deploy_to]}/shared/sphinx/#{rails_env}" unless dir
       file dir,
         :ensure => :directory,
-        :recurse => true,
         :owner => configuration[:user],
         :group => configuration[:group] || configuration[:user]
     end
