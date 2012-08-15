a816653b86ec1b32227416c4f4f9ef5fe5e6cfa2
Josh Nichols <josh@railsmachine.com>
2012-08-15 22:06:25 +0000

    Make sure sphinx tasks run on :sphinx role, add symlink_indexes
    Signed-off-by: Rails Machine <ops@railsmachine.com>

--- recipes/sphinx_cap.rb
+++ recipes/sphinx_cap.rb
@@ -1,27 +1,32 @@
 namespace :sphinx do
-  task :start do
+  task :start, :roles => [:sphinx] do
     sudo "god start #{application}-sphinx || true"
   end
 
-  task :stop do
+  task :stop, :roles => [:sphinx] do
     sudo "god stop #{application}-sphinx || true"
   end
 
-  task :index do
+  task :index, :roles => [:sphinx] do
     run "rake -f #{current_path}/Rakefile ts:in RAILS_ENV=#{fetch(:rails_env, 'production')}"
   end
 
-  task :index_only do
+  task :index_only, :roles => [:sphinx] do
     run "rake -f #{current_path}/Rakefile ts:in INDEX_ONLY=true RAILS_ENV=#{fetch(:rails_env, 'production')}"
   end
 
-  task :configure do
+  task :configure, :roles => [:sphinx] do
     run "rake -f #{current_path}/Rakefile ts:config RAILS_ENV=#{fetch(:rails_env, 'production')}"
   end
 
   desc "Reconfigures sphinx, and then restarts"
-  task :restart do
+  task :restart, :roles => [:sphinx] do
     sudo "god restart #{application}-sphinx || true"
+  end
+
+  desc "Symlink Sphinx indexes"
+  task :symlink_indexes, :roles => [:sphinx] do
+    run "ln -nfs #{shared_path}/sphinx/#{rails_env} #{release_path}/db/sphinx"
   end
 
 end
